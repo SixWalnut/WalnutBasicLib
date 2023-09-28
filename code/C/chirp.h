972cf6d7232305bfa42d95206e2b083d07198626
核核核核核核桃 <43814044+SixWalnut@users.noreply.github.com>
2023-09-28 15:44:12 +0800

    Create chirp.h

---
+++ code/C/chirp.h
@@ -0,0 +1,11 @@
+#ifndef __INCLUDE__CHIRP_H__
+#define __INCLUDE__CHIRP_H__
+
+#ifndef PI
+	#define PI 3.1415926
+#endif
+
+float* chirp(float* DataList, uint32_t ListLenth, double StartFreq, double EndFreq, double UsedTime, double phi);
+#else
+
+#endif
